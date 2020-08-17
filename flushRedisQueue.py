9bfa91435a0b4443c7e0fdcd2a09a34fe2069634
Tejas Parikh <tparikh@redhat.com>
2020-08-17 12:17:15 -0400

    Create flushRedisQueue.py

---
+++ flushRedisQueue.py
@@ -0,0 +1,10 @@
+import redis
+import os
+
+args = {
+    "host": os.getenv('REDIS_HOST', ''),
+    "password": os.getenv('REDIS_AUTH', '')
+    "ssl": True,
+}
+redis_client = redis.StrictRedis(**args)
+redis_client.flushall()
